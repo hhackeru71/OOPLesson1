81239c422d0c73bc754203877e5445972d2329ec
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-20 17:35:13 +0300

    .

--- OOPLesson1/Program.cs
+++ OOPLesson1/Program.cs
@@ -7,6 +7,7 @@ namespace OOPLesson1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("sdfdsf");
         }
     }
 }
