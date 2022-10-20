2dbafce8ec81e43d6a8dacaa58330fe04991b888
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-20 17:50:07 +0300

    .

--- OOPLesson1/Program.cs
+++ OOPLesson1/Program.cs
@@ -6,8 +6,19 @@ namespace OOPLesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("sdfdsf");
+            Console.WriteLine(CheckStringOk("a1233456789"));
+            Console.WriteLine(CheckStringOk("11233456789"));
+
+
+
+
+        }
+        static bool CheckStringOk(string str1)
+        {
+            return str1[0] >= 'a' && str1[0] <= 'z' &&
+                   str1.Length >= 8 &&
+                   str1[str1.Length - 1] >= '0' &&
+                   str1[str1.Length - 1] <= '9';
         }
     }
 }
